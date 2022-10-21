31664c425afed99432fe8e764b936830fed9981b
Arsyad Al Giffa <arsyad.al.giffa94@gmail.com>
2022-10-21 10:39:19 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#06b6d4",
+        secondary: "#64748b",
+        dark: "#1e293b",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
